c893403ae31141ce24bd0508e994523c1d138170
Fouad Matin <fouad@indent.com>
2021-11-09 12:26:53 -0800

    hide: hosted by

--- website/components/footer.tsx
+++ website/components/footer.tsx
@@ -38,7 +38,7 @@ export default function Footer() {
     <footer className={cn(styles.footer)}>
       <div className={styles['footer-legal']}>
         <div className={styles['footer-hostedby']}>
-          <HostedByVercel />
+          {/* <HostedByVercel /> */}
           <div className={styles['footer-separator']} />
         </div>
         <div className={styles['footer-copyright']}>
